9781625571f8db102eb0047c9ee6604533797ba7
Alexander Borovkov <xander27b@gmail.com>
2018-04-30 19:42:13 +0700

    mock exported types for idea

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ export {default as default} from './DuckModule'
 export {combineModules} from './functions'
 export {default as SlashNamedModule} from './SlashNamedModule'
 
+//mock types for IDEA
+export const Action = undefined;
+export const RootSelector = undefined;
+export const ReducerFunction = undefined;
+export const CombinedState  = undefined;
